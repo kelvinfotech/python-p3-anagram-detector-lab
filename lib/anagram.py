3649e833ef6914206588fe0388e9e8869a2b0024
Mwenda <kelvinfotech@gmail.com>
2024-05-31 13:06:19 +0300

    Python P3 Anagram Detector Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,6 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        return [w for w in word_list if sorted(w.lower()) == sorted(self.word)]
